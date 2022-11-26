a077f4bd13cdfa9ee3a9590e16497f9afcc4cf9b
felipyroma <felipy.roma@sptech.school>
2022-11-26 01:46:55 -0300

    ajustes

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,3 +1,4 @@
+// carousel
 const slider = document.querySelectorAll('.slider');
 const btnPrev = document.getElementById('prev-button');
 const btnNext = document.getElementById('next-button');
@@ -14,7 +15,7 @@ function showSlider() {
 
 function nextSlider() {
   hideSlider()
-  if(currentSlide === slider.length -1) {
+  if (currentSlide === slider.length - 1) {
     currentSlide = 0
   } else {
     currentSlide++
@@ -24,8 +25,8 @@ function nextSlider() {
 
 function prevSlider() {
   hideSlider()
-  if(currentSlide === 0) {
-    currentSlide = slider.length -1
+  if (currentSlide === 0) {
+    currentSlide = slider.length - 1
   } else {
     currentSlide--
   }
@@ -43,6 +44,52 @@ currentSlide = currentSlide + 1
 currentSlide = currentSlide - 1
 
 
+
+//funções do quizz
 function logar() {
-    window.location = '../login.html'
+  window.location = "./login.html"
+}
+
+function avançar1() {
+  let sim = document.getElementById('a_sim')
+  let nao = document.getElementById('a_nao')
+
+  if (sim.checked || nao.checked) {
+    quiz_a.style.display = 'none'
+    quiz_b.style.display = 'block'
+
+  }
+}
+
+function avançar2() {
+  let agudo = document.getElementById('b_agudo')
+  let grave = document.getElementById('b_grave')
+
+  if (agudo.checked || grave.checked) {
+    quiz_b.style.display = 'none'
+    quiz_c.style.display = 'block'
+
+  }
+}
+
+function avançar3() {
+  let sozinho = document.getElementById('c_sozinho')
+  let grupo = document.getElementById('c_grupo')
+
+  if (sozinho.checked || grupo.checked) {
+    quiz_c.style.display = 'none'
+    quiz_d.style.display = 'block'
+
+  }
+}
+
+function avançar4() {
+  let melodia = document.getElementById('sozinho')
+  let harmonia = document.getElementById('grupo')
+
+  if (sozinho.checked || grupo.checked) {
+    quiz_d.style.display = 'none'
+    quiz_a.style.display = 'block'
+
+  }
 }
--- site/public/site-institucional/README.md
+++
@@ -1,2 +0,0 @@
-# flute-project
-Projeto individual referente a matéria de PI (Pesquisa e inovação) sobre diferentes tipos de flautas
